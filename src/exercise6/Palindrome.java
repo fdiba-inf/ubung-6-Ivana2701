6d0a6c8d2b4d4acd6259038b1ed22ef32e4fa01c
Ivana2701 <ivanabaj@abv.bg>
2020-11-04 16:17:39 +0000

    .

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -1,9 +1,29 @@
 package exercise6;
-
+ 
 import java.util.Scanner;
-
+ 
 public class Palindrome {
     public static void main(String[] args) {
+ 
         Scanner input = new Scanner(System.in);
+ 
+        Integer new = input.nextInt();
+        String number = num.toString();
+        String reversedNumber = "";
+ 
+        assert num > 0 : "Number is not greater than 0!";
+ 
+        for(int i = number.length()-1; i >= 0; i--){
+            reversedNumber += number.charAt(i);
+        }
+        boolean result = true;
+ 
+        fot(int i = 0; i < number.length(); i++){
+            if(!number.charAt(i) == reversedNumber.charAt(i)){
+                result = false;
+                break;
+            }
+        }
+        System.out.println("Polindrome: " + result);
     }
 }
